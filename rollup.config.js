76f9aca375ee8fdb8f111882c14664977cd4b5c9
MooYeol Prescott Lee <mooyoul@gmail.com>
2021-06-21 07:57:33 +0900

    chore(deps): regular maintenance

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,6 @@
-import typescript from 'rollup-plugin-typescript2';
+import commonjs from '@rollup/plugin-commonjs';
+import nodeResolve from '@rollup/plugin-node-resolve'
+import typescript from '@rollup/plugin-typescript';
 
 export default {
   input: 'index.ts',
@@ -9,6 +11,8 @@ export default {
     sourcemap: true,
   },
   plugins: [
-    typescript(/*{ plugin options }*/)
+    nodeResolve({ browser: true, preferBuiltins: false }),
+    commonjs(),
+    typescript(),
   ],
 };
